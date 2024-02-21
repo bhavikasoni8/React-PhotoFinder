cc8414a142c3d5d135848a2d0d589f2ea9c34701
bhavikasoni8 <bhavikasoni11270@gmail.com>
2024-02-21 13:41:29 +0530

    deployed on firebase

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import logo from '../media/logo.png'
+
+function Navbar() {
+    return (
+        <div className='navbar'>
+            <div className="logo">
+                <img src={logo} alt="" />
+                <p>PicProwl</p>
+            </div>
+            <div className="icons">
+                <div className="icon"><i class="fa-solid fa-bell"></i></div>
+                <div className="icon"><i class="fa-solid fa-comment-dots"></i></div>
+                <div className="icon"><i class="fa-solid fa-user"></i></div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ src/Components/PhotoSearch.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import { createApi } from 'unsplash-js';
+import { TailSpin } from 'react-loader-spinner';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
+
+const unsplash = createApi({
+    accessKey: '3FgGbvS5_jVxjypva4wTqF67RNYVN8N4i3frRFzuPKs'
+})
+
+function PhotoSearch() {
+
+    const [query, setQuery] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [pictures, setPictures] = useState([]);
+
+    const searchPhotos = async (e) => {
+        e.preventDefault();
+        try {
+            setLoading(true)
+            const response = await unsplash.search.getPhotos({
+                query,
+            });
+            if (response.errors) {
+                console.error(response.errors);
+            } else {
+                const photos = response.response.results;
+                setPictures(photos)
+                setLoading(false)
+            }
+        } catch (error) {
+            console.error(error);
+        }
+
+    }
+   
+    return (
+        <div className='pictures_container'>
+            <form className="form" onSubmit={searchPhotos}>
+                <input
+                    type="text"
+                    placeholder={`Try 'India' or 'Flowers' `}
+                    value={query}
+                    onChange={(e) => {
+                        setQuery(e.target.value);
+                    }}
+                />
+                <button type="submit" className="button">
+                    Search
+                </button>
+            </form>
+
+            <div className="cards">
+                {
+                    loading ?
+                        <div className='loading_true'>
+                            <TailSpin />
+                            <p>Loading...</p>
+                        </div>
+                        :
+                        pictures.map((pic, index) => {
+                            return <div key={index} className="card">
+                                <LazyLoadImage src={pic.urls.full} effect="blur" />
+                                {/* <img src={pic.urls.full} loading="lazy"></img> */}
+                            </div>
+                        })
+                }
+            </div>
+        </div>
+    );
+}
+
+export default PhotoSearch;
